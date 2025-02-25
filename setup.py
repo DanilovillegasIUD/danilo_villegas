804b4a4628978348aa7de63daa5248d893cae534
danilo_villegas <danilo.villegas@est.iudigital.edu.co>
2025-02-25 11:27:22 -0500

    Actividad 1 - configuracion

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Actividad_1_A_D",
+    version="0.0.1",
+    author="Danilo Villegas",
+    author_email="danilo.villegas@est.iudigital.edu.co",
+    description="Evidencia de aprendizaje Clases, Objetos y archivos de datos en Python",
+    py_modules=["A1_AD_VILLEGAS"],
+    install_requires=[
+        "pandas",
+        "matplotlib"
+    ]
+    
+    
+)
+
