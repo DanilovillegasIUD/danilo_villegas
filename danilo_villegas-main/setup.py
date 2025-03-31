f0754092a5fd86daf21b5cc155acb75113c15c18
danilo_villegas <danilo.villegas@est.iudigital.edu.co>
2025-03-31 00:09:08 -0500

    nueva carga

---
+++ danilo_villegas-main/setup.py
@@ -0,0 +1,23 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Actividad_3",
+    version="0.0.2",
+    author="Danilo Villegas Restrepo",
+    author_email="danilo.villegas@est.iudigital.edu.co",
+    description="Evidencia de aprendizaje Numero 3",
+    py_modules=["Actividad_3"],
+    install_requires=[
+        "kagglehub[pandas-datasets]>=0.3.8",
+        "matplotlib>=3.5.0",
+        "seaborn>=0.11.2",
+        "pandas",
+        "numpy",
+        "requests"
+    ]
+    
+    
+)
+
