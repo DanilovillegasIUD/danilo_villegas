126c123d8a6c8e5eef81c011d9dee9de754f673f
DanilovillegasIUD <danilo.villegas@est.iudigital.edu.co>
2025-03-31 18:27:01 -0500

    Update Actividad_3.py

--- ACTIVIDAD_3/Actividad_3.py
+++ ACTIVIDAD_3/Actividad_3.py
@@ -121,11 +121,11 @@ print("-->> Dataframe del punto 9 con los vinos de california, se guardó en .cs
 max_precio = review2['price'].idxmax()
 vino_caro = review2.loc[max_precio]
 
-vino_Caro_dataframe = pd.DataFrame([vino_caro])
+vino_caro_dataframe = pd.DataFrame([vino_caro])
 
-vino_Caro_dataframe.to_csv("ACTIVIDAD_3/Punto_10.csv")
+vino_caro_dataframe.to_csv("ACTIVIDAD_3/Punto_10.csv")
 
-print("la Informacion del vino mas caro es: ",vino_caro)
+print(vino_caro)
 
 print("-->> Dataframe del punto 10 con la informacion completa del vino mas caro, se guardó en .csv con exito en la carpeta Actividad_3")
 
