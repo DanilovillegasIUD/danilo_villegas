85bcd87a4f9e6eb98986a15cff056c942a356551
DanilovillegasIUD <danilo.villegas@est.iudigital.edu.co>
2025-04-03 20:13:59 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Actividad_3",
-    version="0.0.2",
+    name="Actividad_3 y Final",
+    version="0.0.4",
     author="Danilo Villegas Restrepo",
     author_email="danilo.villegas@est.iudigital.edu.co",
-    description="Evidencia de aprendizaje Numero 3",
-    py_modules=["Actividad_3"],
+    description="Evidencia de aprendizaje Numero 3 y Final",
+    py_modules=["Actividad_3", "Actividad_Final"],
     install_requires=[
         "kagglehub[pandas-datasets]>=0.3.8",
         "matplotlib>=3.5.0",
         "seaborn>=0.11.2",
         "pandas",
         "numpy",
+        "openpyxl",
         "requests"
     ]
     
